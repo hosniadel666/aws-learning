50cd72b4d4fffe8c3e3f6588321019b54f49e7e2
hosniadel666 <hosni.adel666@gmail.com>
2025-12-06 09:10:32 -0800

    Upload file to bucket

--- main.tf
+++ main.tf
@@ -18,12 +18,16 @@ resource "aws_s3_bucket_versioning" "versioning" {
   }
 }
 
-# Upload sample data
-resource "aws_s3_object" "sample_data" {
-  for_each = fileset("${path.module}/sample_data", "*")
+resource "aws_s3_bucket_object" "sample_data" {
+  for_each = { for f in fileset("${path.module}/sample_data", "*") : f => f }
 
   bucket = aws_s3_bucket.data_bucket.id
   key    = each.value
   source = "${path.module}/sample_data/${each.value}"
+
+  # Optional: re-upload if file changes
+  etag = filemd5("${path.module}/sample_data/${each.value}")
+
+  depends_on = [aws_s3_bucket.data_bucket]
 }
 
